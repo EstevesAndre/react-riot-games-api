f747f0e6ff1387f88a0fe0c661352a1a6732d622
Andre Esteves <estevesandre1@hotmail.com>
2020-08-27 02:16:09 +0100

    Create cookies handlers for language, region and UID

---
+++ client/src/api/cookies.js
@@ -0,0 +1,88 @@
+import Cookies from "js-cookie";
+import api from "./";
+
+const languages = ["en", "fr", "es", "pt", "kr"];
+const regions = [
+  "euw",
+  "kr",
+  "jp",
+  "na",
+  "eune",
+  "oce",
+  "br",
+  "las",
+  "lan",
+  "ru",
+  "tr",
+];
+
+export const updateLanguageCookie = (lang) => {
+  const cLang = Cookies.get("Language");
+
+  // Checks valid language
+  if (languages.includes(lang)) {
+    if (cLang !== lang) {
+      Cookies.set("Language", lang);
+    }
+    return "correct";
+  } else {
+    if (cLang !== undefined) {
+      return cLang;
+    } else {
+      Cookies.set("Language", "en");
+      return "en";
+    }
+  }
+};
+
+export const updateRegionCookie = (region) => {
+  // Checks valid region
+  const cRegion = Cookies.get("Region");
+
+  if (regions.includes(region)) {
+    if (cRegion !== region) {
+      Cookies.set("Region", region);
+    }
+    return "correct";
+  } else {
+    if (cRegion !== undefined) {
+      return cRegion;
+    } else {
+      Cookies.set("Region", "euw");
+      return "euw";
+    }
+  }
+};
+
+export const updateHeaderCookies = (lang, region) => {
+  const up1Status = updateLanguageCookie(lang);
+  const up2Status = updateRegionCookie(region);
+
+  if (up1Status === "correct" && up2Status === "correct") {
+    return { status: true };
+  } else if (up1Status === "correct") {
+    return { status: false, pathParams: `/${lang}/${up2Status}` };
+  } else if (up2Status === "correct") {
+    return { status: false, pathParams: `/${up1Status}/${region}` };
+  } else {
+    return { status: false, pathParams: `/${up1Status}/${up2Status}` };
+  }
+};
+
+export const createNewUser = async () => {
+  const res = await api.insertUser();
+
+  if (res.data.success) {
+    console.log(res.data.message);
+    Cookies.set("UID", res.data.id.toString());
+  }
+};
+
+const cookiesApi = {
+  updateLanguageCookie,
+  updateRegionCookie,
+  updateHeaderCookies,
+  createNewUser,
+};
+
+export default cookiesApi;
