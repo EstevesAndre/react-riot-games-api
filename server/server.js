d806394b90134da51eb8dce05d86feaccc18132c
Andre Esteves <estevesandre1@hotmail.com>
2020-08-31 00:01:57 +0100

    Add heroku dependencies

---
+++ server/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const pino = require("express-pino-logger")();
+const dotenv = require("dotenv");
+
+const db = require("./src/db");
+const userRouter = require("./src/routes/user");
+const riotRouter = require("./src/routes/riot");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(pino);
+
+dotenv.config();
+
+const port = process.env.PORT || 5001;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+app.get("/api/greeting", (req, res) => {
+  const name = req.query.name || "World";
+  res.setHeader("Content-Type", "application/json");
+  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use("/api/riot", riotRouter);
+app.use("/api", userRouter);
+
+app.listen(port, () =>
+  console.log(`Express server is running on localhost:${port}`)
+);
