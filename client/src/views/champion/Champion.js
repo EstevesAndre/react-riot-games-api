04585baf235a4368368b32eaf6dab6ad062bac2c
Andre Esteves <estevesandre1@hotmail.com>
2020-09-04 14:37:44 +0100

    Test build error

--- client/src/views/champion/Champion.js
+++ client/src/views/champion/Champion.js
@@ -16,7 +16,7 @@ import { Slider } from "rsuite";
 
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import BaseNavbar from "../../components/navbar";
-import { ImageGroup, Image } from "react-fullscreen-image";
+// import { ImageGroup, Image } from "react-fullscreen-image";
 
 import "react-tabs/style/react-tabs.css";
 import "./Champion.css";
@@ -519,7 +519,7 @@ const SkinsTab = (props) => {
 
   return (
     <div className="images-container">
-      <ImageGroup transitionMs="750">
+      {/* <ImageGroup transitionMs="750">
         <ul className="images">
           {champion.skins.map((skin, index) => (
             <li key={index}>
@@ -534,7 +534,7 @@ const SkinsTab = (props) => {
             </li>
           ))}
         </ul>
-      </ImageGroup>
+      </ImageGroup> */}
     </div>
   );
 };
