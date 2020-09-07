c1c36bc9a79d1c1d933f4f7dc1897b73fc97eb29
Andre Esteves <estevesandre1@hotmail.com>
2020-09-07 20:15:08 +0100

    Update navbar layout

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,152 +1,102 @@
 import React, { useState } from "react";
-import Switch from "react-switch";
-import Cookies from "js-cookie";
 
-// reactstrap components
-import {
-  Collapse,
-  NavbarBrand,
-  Navbar,
-  NavItem,
-  NavLink,
-  Nav,
-  NavbarToggler,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-} from "reactstrap";
-
-import Logo from "../../assets/league_logo.svg";
-// import LogoB from "../../assets/league_logo_black.svg";
-import Globe from "../../assets/earth.svg";
+import { NavbarBrand, Navbar, NavItem, NavLink, Nav } from "reactstrap";
+import { Drawer, Icon, IconButton } from "rsuite";
 
 import SearchSummonerForm from "../searchSummonerForm";
 
+import Logo from "../../assets/league_logo.svg";
 import "./Navbar.css";
 
 const BaseNavbar = (props) => {
-  const [darkModeSwitchChecked, setDarkModeSwitchChecked] = useState(
-    Cookies.get("dark") === "true" ? true : false
-  );
-  const [isOpen, setIsOpen] = useState(false);
-  const [language, setLanguage] = useState(props.match.params.lang || "en");
-  const languages = [
-    {
-      id: 0,
-      name: "English",
-      paramCode: "en",
-    },
-    {
-      id: 1,
-      name: "French",
-      paramCode: "fr",
-    },
-    {
-      id: 2,
-      name: "Spanish",
-      paramCode: "es",
-    },
-    {
-      id: 3,
-      name: "Portuguese",
-      paramCode: "pt",
-    },
-    {
-      id: 4,
-      name: "Korea",
-      paramCode: "kr",
-    },
-  ];
-
-  const switchDarkMode = () => {
-    Cookies.set("dark", darkModeSwitchChecked ? false : true);
-    setDarkModeSwitchChecked(!darkModeSwitchChecked);
-  };
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const language = props.match.params.lang || "en";
 
   return (
     <>
       <header>
-        <Navbar dark className="app-navbar navbar-1-color" expand="lg">
+        <Navbar dark className="app-navbar app-navbar-color" expand="lg">
           <div className="nav-container">
-            <NavbarBrand href="/">LOGO</NavbarBrand>
+            <NavbarBrand href="/">
+              <img className="header-icon" src={Logo} alt="logo" />
+            </NavbarBrand>
             <Nav className="mr-auto" navbar>
-              <NavItem className="header-item">
-                <NavLink active href="/">
-                  <img className="header-icon" src={Logo} alt="logo" />
-                  League of Legends
-                </NavLink>
-              </NavItem>
-              <NavItem className="header-item">
-                <NavLink>
-                  <img className="header-icon" src={Logo} alt="logo" />
-                  Teamfight Tactics
-                </NavLink>
-              </NavItem>
-            </Nav>
-            <Switch
-              onChange={() => switchDarkMode()}
-              checked={darkModeSwitchChecked}
-              uncheckedIcon={false}
-              checkedIcon={false}
-              onColor="#898"
-              offColor="#898"
-              onHandleColor="#fff"
-              offHandleColor="#000"
-              activeBoxShadow="0 0 2px 3px #565"
-            />
-            <div className="language-container ml-3">
-              <UncontrolledDropdown inNavbar>
-                <DropdownToggle caret>
-                  <img src={Globe} alt="Logo" />
-                  {language}
-                </DropdownToggle>
-                <DropdownMenu color="inherit" right className="menu-dropdown">
-                  {languages.map((language) => (
-                    <DropdownItem
-                      key={language.id}
-                      onClick={() => setLanguage(language.paramCode)}
-                    >
-                      <span>{language.name}</span>
-                    </DropdownItem>
-                  ))}
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </div>
-          </div>
-        </Navbar>
-      </header>
-      <Navbar dark className="navbar-2-color" expand="lg">
-        <div className="navbar-with-search">
-          <NavbarToggler onClick={() => setIsOpen(!isOpen)} />
-          <Collapse isOpen={isOpen} navbar>
-            <Nav className="mr-auto nav-links" navbar>
-              <NavItem>
+              <NavItem className="mobile-content-center">ICON</NavItem>
+              <NavItem className="nav-content-link">
                 <NavLink
+                  className={props.page === "champions" ? "active" : ""}
                   href={`/${language}/${props.match.params.region}/champions`}
                 >
                   Champions
                 </NavLink>
               </NavItem>
-              <NavItem>
+              <NavItem className="nav-content-link">
                 <NavLink
+                  className={props.page === "leaderboard" ? "active" : ""}
                   href={`/${language}/${props.match.params.region}/leaderboard`}
                 >
                   Leaderboard
                 </NavLink>
               </NavItem>
-              <NavItem>
+              <NavItem className="nav-content-link">
                 <NavLink
+                  className={props.page === "statistics" ? "active" : ""}
+                  href={`/${language}/${props.match.params.region}/statistics`}
+                >
+                  Statistics
+                </NavLink>
+              </NavItem>
+              <NavItem className="nav-content-link">
+                <NavLink
+                  className={props.page === "items" ? "active" : ""}
                   href={`/${language}/${props.match.params.region}/items`}
                 >
                   Items
                 </NavLink>
               </NavItem>
             </Nav>
+            <div className="nav-content-link">
+              {!props.noSearch && <SearchSummonerForm {...props} />}
+            </div>
+            <div className="drawer-button">
+              <IconButton
+                circle
+                icon={<Icon icon="bars" />}
+                onClick={() => setDrawerOpen(true)}
+              />
+            </div>
+          </div>
+        </Navbar>
+        <Drawer
+          size="xs"
+          placement="right"
+          show={drawerOpen}
+          className="navbar-drawer"
+          enforceFocus={true}
+          keyboard={true}
+          onHide={() => setDrawerOpen(false)}
+        >
+          <Drawer.Header>
+            <Drawer.Title>Drawer Title</Drawer.Title>
+          </Drawer.Header>
+          <Drawer.Body className="drawer-link-list">
+            <a href={`/${language}/${props.match.params.region}`}>Home</a>
+            <a href={`/${language}/${props.match.params.region}/champions`}>
+              Champions
+            </a>
+            <a href={`/${language}/${props.match.params.region}/leaderboard`}>
+              Leaderboard
+            </a>
+            <a href={`/${language}/${props.match.params.region}/statistics`}>
+              Statistics
+            </a>
+            <a href={`/${language}/${props.match.params.region}/items`}>
+              Items
+            </a>
             {!props.noSearch && <SearchSummonerForm {...props} />}
-          </Collapse>
-        </div>
-      </Navbar>
+          </Drawer.Body>
+        </Drawer>
+      </header>
     </>
   );
 };
