92d51f4a00dbaca29b936e806b88de0a78e726ea
Andre Esteves <estevesandre1@hotmail.com>
2020-09-08 22:54:16 +0100

    Add grid to champions list and border in img

--- client/src/views/champions/Champions.js
+++ client/src/views/champions/Champions.js
@@ -3,8 +3,11 @@ import ReactTooltip from "react-tooltip";
 import Cookies from "js-cookie";
 import axios from "axios";
 
+import { Grid, Row, Col } from "rsuite";
+
 import BaseNavbar from "../../components/navbar";
 import FreeChampRotation from "../../components/freeChampRotation";
+import Footer from "../../components/footer";
 
 import "./Champions.css";
 
@@ -38,44 +41,58 @@ const Champions = (props) => {
   return (
     <div>
       <BaseNavbar {...props} page="champions" />
-      <div className="one-page-plus">
-        <h1 className=""> CHAMPIONS </h1>
-        <div
-          className="champs-list p-2"
-          version={loading ? "" : champions.version}
-        >
-          {champions.map((champ, index) => (
-            <a key={index} href={`champion/${champ.id}`}>
-              {loading ? (
-                <div>
-                  <div className="default-img"></div>
-                  <div className="champ-name">Champion</div>
-                </div>
-              ) : (
-                <div>
-                  <img
-                    data-tip
-                    data-for={`champion${champ.key}tip`}
-                    src={`https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/${champ["image"].full}`}
-                    alt="full-champion"
-                  />
-                  <br />
-                  <div className="champ-name">{champ.name}</div>
-                  <ReactTooltip
-                    id={`champion${champ.key}tip`}
-                    className="champ-tooltip"
-                    place="top"
-                    effect="solid"
-                  >
-                    {champ.title}
-                  </ReactTooltip>
-                </div>
-              )}
-            </a>
-          ))}
+      <div className="one-page-plus-minus-nav-minus-footer bg-1">
+        <h1 className="mt-3"> Champions </h1>
+        <h6 className="mt-3">
+          Checkout the list of all league of legends champions
+          <br />
+          <small>Discover more about every champion</small>
+        </h6>
+        <Grid fluid>
+          <Row>
+            <Col xs={22} xsOffset={1} md={18} mdOffset={3} lg={14} lgOffset={5}>
+              <div
+                className="champs-list py-2"
+                version={loading ? "" : champions.version}
+              >
+                {champions.map((champ, index) => (
+                  <a key={index} href={`champion/${champ.id}`}>
+                    {loading ? (
+                      <div>
+                        <div className="default-img"></div>
+                        <div className="champ-name">-</div>
+                      </div>
+                    ) : (
+                      <div>
+                        <img
+                          data-tip
+                          data-for={`championDesc${champ.key}tip`}
+                          src={`https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/${champ["image"].full}`}
+                          alt="full-champion"
+                        />
+                        <br />
+                        <div className="champ-name">{champ.name}</div>
+                        <ReactTooltip
+                          id={`championDesc${champ.key}tip`}
+                          className="champ-tooltip"
+                          place="top"
+                          effect="solid"
+                        >
+                          {champ.title}
+                        </ReactTooltip>
+                      </div>
+                    )}
+                  </a>
+                ))}
+              </div>
+            </Col>
+          </Row>
+        </Grid>
+        <div className="mb-4">
+          <FreeChampRotation {...props} />
         </div>
-        <FreeChampRotation {...props} />
       </div>
+      <Footer {...props} page={"champions"} />
     </div>
   );
 };
