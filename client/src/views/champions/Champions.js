1359dd700517acc53aa1ad0cf39fc317de037754
Andre Esteves <estevesandre1@hotmail.com>
2020-09-11 12:48:06 +0100

    Add loader to champions page

--- client/src/views/champions/Champions.js
+++ client/src/views/champions/Champions.js
@@ -2,8 +2,10 @@ import React, { useState, useEffect } from "react";
 import ReactTooltip from "react-tooltip";
 import Cookies from "js-cookie";
 import axios from "axios";
+import { ddragonVersion } from "../../constants";
 
 import { Grid, Row, Col } from "rsuite";
+import Loader from "react-loader-spinner";
 
 import BaseNavbar from "../../components/navbar";
 import FreeChampRotation from "../../components/freeChampRotation";
@@ -20,7 +22,7 @@ const Champions = (props) => {
   useEffect(() => {
     axios
       .get(
-        "https://ddragon.leagueoflegends.com/cdn/10.16.1/data/en_US/champion.json"
+        `https://ddragon.leagueoflegends.com/cdn/${ddragonVersion}/data/en_US/champion.json`
       )
       .then((res) => {
         Cookies.set("championCount", Object.keys(res.data.data).length);
@@ -48,49 +50,64 @@ const Champions = (props) => {
           <br />
           <small>Discover more about every champion</small>
         </h6>
-        <Grid fluid>
-          <Row>
-            <Col xs={22} xsOffset={1} md={18} mdOffset={3} lg={14} lgOffset={5}>
-              <div
-                className="champs-list py-2"
-                version={loading ? "" : champions.version}
-              >
-                {champions.map((champ, index) => (
-                  <a key={index} href={`champion/${champ.id}`}>
-                    {loading ? (
-                      <div>
-                        <div className="default-img"></div>
-                        <div className="champ-name">-</div>
-                      </div>
-                    ) : (
-                      <div>
-                        <img
-                          data-tip
-                          data-for={`championDesc${champ.key}tip`}
-                          src={`https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/${champ["image"].full}`}
-                          alt="full-champion"
-                        />
-                        <br />
-                        <div className="champ-name">{champ.name}</div>
-                        <ReactTooltip
-                          id={`championDesc${champ.key}tip`}
-                          className="champ-tooltip"
-                          place="top"
-                          effect="solid"
-                        >
-                          {champ.title}
-                        </ReactTooltip>
-                      </div>
-                    )}
-                  </a>
-                ))}
-              </div>
-            </Col>
-          </Row>
-        </Grid>
-        <div className="mb-4">
-          <FreeChampRotation {...props} />
-        </div>
+        {loading ? (
+          <div className="loader-container">
+            <Loader type="ThreeDots" color="#d13639" height={100} width={100} />
+          </div>
+        ) : (
+          <div>
+            <Grid fluid>
+              <Row>
+                <Col
+                  xs={22}
+                  xsOffset={1}
+                  md={18}
+                  mdOffset={3}
+                  lg={14}
+                  lgOffset={5}
+                >
+                  <div
+                    className="champs-list py-2"
+                    version={loading ? "" : champions.version}
+                  >
+                    {champions.map((champ, index) => (
+                      <a key={index} href={`champion/${champ.id}`}>
+                        {loading ? (
+                          <div>
+                            <div className="default-img"></div>
+                            <div className="champ-name">-</div>
+                          </div>
+                        ) : (
+                          <div>
+                            <img
+                              data-tip
+                              data-for={`championDesc${champ.key}tip`}
+                              src={`https://ddragon.leagueoflegends.com/cdn/${ddragonVersion}/img/champion/${champ["image"].full}`}
+                              alt="full-champion"
+                            />
+                            <br />
+                            <div className="champ-name">{champ.name}</div>
+                            <ReactTooltip
+                              id={`championDesc${champ.key}tip`}
+                              className="champ-tooltip"
+                              place="top"
+                              effect="solid"
+                            >
+                              {champ.title}
+                            </ReactTooltip>
+                          </div>
+                        )}
+                      </a>
+                    ))}
+                  </div>
+                </Col>
+              </Row>
+            </Grid>
+            <div className="mb-4">
+              <FreeChampRotation {...props} />
+            </div>
+          </div>
+        )}
       </div>
       <Footer {...props} page={"champions"} />
     </div>
