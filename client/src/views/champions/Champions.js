6874830afd588390c2c4c0bc8e53df5133b3238b
Andre Esteves <estevesandre1@hotmail.com>
2020-09-11 21:54:35 +0100

    Champions data stored in localStorage

--- client/src/views/champions/Champions.js
+++ client/src/views/champions/Champions.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import ReactTooltip from "react-tooltip";
-import Cookies from "js-cookie";
 import axios from "axios";
 import { ddragonVersion } from "../../constants";
 
@@ -15,45 +14,96 @@ import "./Champions.css";
 
 const Champions = (props) => {
   const [loading, setLoading] = useState(true);
-  const [champions, setChampions] = useState(
-    Array.from({ length: Cookies.get("championCount") }, (v, i) => i)
-  );
+  const [error, setError] = useState(null);
+  const [champions, setChampions] = useState([]);
 
   useEffect(() => {
-    axios
-      .get(
-        `https://ddragon.leagueoflegends.com/cdn/${ddragonVersion}/data/en_US/champion.json`
-      )
-      .then((res) => {
-        Cookies.set("championCount", Object.keys(res.data.data).length);
-        let champs = [];
-        for (var ch in res.data.data) {
-          if (res.data.data.hasOwnProperty(ch)) {
-            champs.push(res.data.data[ch]);
-          }
+    async function getChampionsInfo() {
+      const res = await axios
+        .get(
+          `https://ddragon.leagueoflegends.com/cdn/${ddragonVersion}/data/en_US/champion.json`
+        )
+        .catch((err) => {
+          console.log(err);
+        });
+
+      if (res === undefined) {
+        setError("Unable to retrive champion information. Try again later.");
+        return;
+      }
+
+      let champs = [];
+      for (var ch in res.data.data) {
+        if (res.data.data.hasOwnProperty(ch)) {
+          champs.push({
+            id: res.data.data[ch].id,
+            name: res.data.data[ch].name,
+            key: res.data.data[ch].key,
+            title: res.data.data[ch].title,
+            imageFull: res.data.data[ch]["image"].full,
+          });
         }
-        setChampions(champs);
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+      }
+      setChampions(champs);
+      localStorage.setItem("champs-basic-info", JSON.stringify(champs));
+      setLoading(false);
+    }
+
+    async function checkChampionsCountToUpdateLocalStorage(actualSize) {
+      const res = await axios
+        .get(
+          `https://ddragon.leagueoflegends.com/cdn/${ddragonVersion}/data/en_US/champion.json`
+        )
+        .catch((err) => {
+          console.log(err);
+        });
+
+      if (res === undefined) {
+        return;
+      }
+
+      if (Object.keys(res.data.data).length !== actualSize) {
+        setLoading(true);
+        getChampionsInfo();
+      }
+    }
+
+    const cachedHits = localStorage.getItem("champs-basic-info");
+    if (cachedHits) {
+      setChampions(JSON.parse(cachedHits));
+      setLoading(false);
+      checkChampionsCountToUpdateLocalStorage(JSON.parse(cachedHits).length);
+    } else {
+      getChampionsInfo();
+    }
   }, []);
 
   return (
     <div>
       <BaseNavbar {...props} page="champions" />
       <div className="one-page-plus-minus-nav-minus-footer bg-1">
-        <h1 className="mt-3"> Champions </h1>
+        <h1 className="pt-3"> Champions </h1>
         <h6 className="mt-3">
           Checkout the list of all league of legends champions
           <br />
           <small>Discover more about every champion</small>
         </h6>
-        {loading ? (
-          <div className="loader-container">
-            <Loader type="ThreeDots" color="#d13639" height={100} width={100} />
+        {error && (
+          <div className="py-5">
+            <h4>{error}</h4>
           </div>
+        )}
+        {loading ? (
+          error === null && (
+            <div className="loader-container">
+              <Loader
+                type="ThreeDots"
+                color="#d13639"
+                height={100}
+                width={100}
+              />
+            </div>
+          )
         ) : (
           <div>
             <Grid fluid>
@@ -66,37 +116,27 @@ const Champions = (props) => {
                   lg={14}
                   lgOffset={5}
                 >
-                  <div
-                    className="champs-list py-2"
-                    version={loading ? "" : champions.version}
-                  >
+                  <div className="champs-list py-2" version={champions.version}>
                     {champions.map((champ, index) => (
                       <a key={index} href={`champion/${champ.id}`}>
-                        {loading ? (
-                          <div>
-                            <div className="default-img"></div>
-                            <div className="champ-name">-</div>
-                          </div>
-                        ) : (
-                          <div>
-                            <img
-                              data-tip
-                              data-for={`championDesc${champ.key}tip`}
-                              src={`https://ddragon.leagueoflegends.com/cdn/${ddragonVersion}/img/champion/${champ["image"].full}`}
-                              alt="full-champion"
-                            />
-                            <br />
-                            <div className="champ-name">{champ.name}</div>
-                            <ReactTooltip
-                              id={`championDesc${champ.key}tip`}
-                              className="champ-tooltip"
-                              place="top"
-                              effect="solid"
-                            >
-                              {champ.title}
-                            </ReactTooltip>
-                          </div>
-                        )}
+                        <div>
+                          <img
+                            data-tip
+                            data-for={`championDesc${champ.key}tip`}
+                            src={`https://ddragon.leagueoflegends.com/cdn/${ddragonVersion}/img/champion/${champ.imageFull}`}
+                            alt="full-champion"
+                          />
+                          <br />
+                          <div className="champ-name">{champ.name}</div>
+                          <ReactTooltip
+                            id={`championDesc${champ.key}tip`}
+                            className="champ-tooltip"
+                            place="top"
+                            effect="solid"
+                          >
+                            {champ.title}
+                          </ReactTooltip>
+                        </div>
                       </a>
                     ))}
                   </div>
